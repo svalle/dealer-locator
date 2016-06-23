c23e80e05adc22d016ae842d48323bccd8a68eb5
dmora-oshyn <dmora@oshyn.com>
2016-06-23 11:33:24 -0500

    cambios de estilos para algunos elementos

--- js/dealer-locator.js
+++ js/dealer-locator.js
@@ -841,21 +841,18 @@ var DealerLocator = (function () {
                 k++
             });
 
+			//assign make/remove preferred to anchors
             assignMakePreferred();
             assignRemovePreferred();
 			
-			// reorder preferred
+			// reorder preferred after view more
 			if ($preferredDealer) {        
 
 				var $parentList = $listItem.closest('.results-list');     						
 				var $arrayList = $parentList.find('.results-item-template');
-				var $arrayIndex = 0;											
+				var $arrayIndex = 0;																			
 				
-				//console.log('$listItem: '+ $listItem.attr('class'));	
-				
-				$parentList.find('.dealer-result-dealerNumber').each(function(index){
-				//$('.main-content .dealer-result-dealerNumber').each(function(index){
-				//console.log( index + ": " + $( this ).text() + ' list-item parent: ' + $listItem.closest('.results-list').attr('class') );
+				$parentList.find('.dealer-result-dealerNumber').each(function(index){				
 					
 				  if($(this).text()== $preferredDealer){ 
 					$arrayIndex = $(this).closest('.row.results-item-template').index();		
